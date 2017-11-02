bfbbc042b7ccfff81bd84729076deccc1efe973c
Sanju Choudhary <mail2sanju@gmail.com>
2017-11-02 09:09:52 +0530

    Test hiroku port x1

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,12 @@
 var env = process.env.NODE_ENV || 'development';
 
 if(env === 'development') {
-  process.env.PORT = 3000;
+  process.env.PORT = 3001;
   process.env.MONGODB_URI =  'mongodb://localhost:27017/TodoApp';
 } else if(env === 'test') {
-  process.env.PORT = 3000;
+  process.env.PORT = 3002;
   process.env.MONGODB_URI =  'mongodb://localhost:27017/TodoAppTest';
+} else if (env === 'production') {
+  // heroku handle the vars
+  process.env.PORT = 3003;
 }
